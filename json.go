33e5b6085650dff3a52e7b914784831438e6bd42
SergioFloresCorrea <sergio.flores.c9029@gmail.com>
2025-05-02 21:31:32 -0500

    Added a continuous development (cd.yml) file

--- json.go
+++ json.go
@@ -33,6 +33,5 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	_, err = w.Write(dat)
 	if err != nil {
 		log.Printf("Error in writing the response: %s", err)
-		return
 	}
 }
